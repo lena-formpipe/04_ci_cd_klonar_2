cd9b4e577d130b8715777db573c1833f1d80492c
Lena S <lena.i.sandberg@gmail.com>
2026-08-27 11:06:56 +0200

    försöker få bort filer

--- tests/integration/test_event_integration.py
+++
@@ -1,7 +0,0 @@
-# register_new_member GÖR TVÅ SAKER
-# - registrerar en ny medlem i klubben, om sådan inte redan finns
-# - OCH skriver upp denna på arrangemanget
-# skapa INTEGRATION TEST: anropar metod i klassen Member_service
-# ingen mock e.dyl behövs då metoderna SKA interagera direkt
-def test_register_new_member(self):
-    pass
--- tests/unit/test_event_unit.py
+++ tests/unit/test_event_unit.py
@@ -1,12 +1,11 @@
-from src.uppgift2.event import Event
+import pytest
+# behöver inte importera det som används via fixtures
 
-# sign_up
-# Lägger till en medlem på arrangemanget
-def test_sign_up():
-    # arrange
-    # skapa objekt Event och en member
-    event = Event("Utflykt")
-    member1 = "Stina"
+# sign_up - Lägger till en medlem på arrangemanget
+@pytest.mark.unit
+def test_sign_up(event, member1):
+    # arrange - se fixtures
     # act
     event.sign_up(member1)
+    # assert
     assert member1 in event.members_on_event
