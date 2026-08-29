f566b532ae042f500c6e9da785f1e64f8ef9ef00
Lena S <lena.i.sandberg@gmail.com>
2026-08-29 17:02:07 +0200

    spårbara transaktioner @fixtures

--- src/transaktioner/bank_account.py
+++ src/transaktioner/bank_account.py
@@ -2,29 +2,30 @@ from src.transaktioner.logger import Logger
 
 # Dependency Injection: BankAccount tar emot en logger istället för att skapa en egen
 # På så sätt kan vi få alla bankkonton loggade i samma logger.
+# om inget belopp skickas med är default 0 kr i balans när konto skapas.
 class BankAccount:
     def __init__(self, customer_name:str, logger:Logger, balance= 0,  ):
         self._name = customer_name
-        self._balance = balance # när konto skapas är det 0 kr i balans
+        self._balance = balance
         self._logger = logger
 
 
     def deposit(self, amount:float):
         if amount <= 0:
-            self._logger.log(f"Deposit: {amount} must higher than 0.")
+            self._logger.log(f"Deposit: {amount} måste vara större än 0.")
         elif amount >0:
             self._balance += amount
             # uppgiften: logger ska skriva "deposit: X kr, saldo X kr"
-            self._logger.log(f"Deposit: {amount}, saldo after deposit {self._balance} kr.")
+            self._logger.log(f"Deposit: {amount} kr, saldo efter deposit {self._balance} kr.")
 
 
     def withdraw(self, amount:float ):
         # gör uttag om pengar finns
         if self._balance >= amount:
             self._balance -= amount
-            self._logger.log(f"Withdraw: {amount}, saldo after withdraw {self._balance} kr.")
+            self._logger.log(f"Withdraw: {amount} kr, saldo efter uttag {self._balance} kr.")
             return True
         # enbart loggning om pengar inte finns
         else:
-            self._logger.log(f"Withdraw: could not withdraw {amount} from account.")
+            self._logger.log(f"Withdraw: kunde inte ta ut {amount} kr från kontot.")
             return False
--- tests/integration/test_bank_account.py
+++ tests/integration/test_bank_account.py
@@ -1,15 +1,35 @@
-import pytest_mock as mocker
+import pytest
 
 from src.transaktioner.bank_account import BankAccount
 from src.transaktioner.transaction import Transaction
 from src.transaktioner.logger import Logger
 
 
-def test_bank_account_deposit(mocker):
-    # arrange
-    logger = Logger()
-    amount1 = 100
+@pytest.fixture
+def bank_account1(logger):
     bank_account1 = BankAccount("Kalles konto", logger)
+    return bank_account1
+
+@pytest.fixture
+def bank_account2(logger):
+    bank_account2 = BankAccount("Annas konto", logger)
+    return bank_account2
+
+
+@pytest.fixture()
+def logger():
+    logger = Logger()
+    return logger
+
+@pytest.fixture()
+def transaction():
+    transaction = Transaction()
+    return transaction
+
+
+def test_bank_account_deposit(mocker, bank_account1, logger):
+    # arrange
+    amount1 = 100
     spy = mocker.spy(logger, 'log')
     # act
     bank_account1.deposit(amount1)
@@ -19,41 +39,33 @@ def test_bank_account_deposit(mocker):
     spy.assert_called_once()
 
 
-def test_transaction_transfer__cannot_transfer_amount(mocker):
+def test_transaction_transfer__cannot_transfer_amount(mocker, bank_account1, bank_account2, logger, transaction):
     # arrange
-    logger = Logger()
-    transaktion = Transaction()
     spy = mocker.spy(logger, 'log')
     amount1 = 100
     amount_to_transfer = 101
-    bank_account1 = BankAccount("Kalles konto", logger, amount1)
-    bank_account2 = BankAccount("Annas konto", logger)
+    bank_account1.deposit(amount1)
     # act
-    transaktion.transfer(amount_to_transfer, bank_account1, bank_account2)
+    transaction.transfer(amount_to_transfer, bank_account1, bank_account2)
     assert bank_account1._balance == amount1
     assert bank_account2._balance == 0
-    spy.assert_called_once()
+    assert spy.call_count == 2
 
 
-def test_transaction_transfer__amount_is_transferred(mocker):
+def test_transaction_transfer__amount_is_transferred(mocker, logger, transaction, bank_account1, bank_account2):
     # arrange
-    logger = Logger()
-    transaktion = Transaction()
     spy = mocker.spy(logger, 'log')
     amount1 = 100
+    bank_account1.deposit(amount1)
     amount_to_transfer = 91
-    bank_account1 = BankAccount("Kalles konto", logger, amount1)
-    bank_account2 = BankAccount("Annas konto", logger)
     # act
-    transaktion.transfer(amount_to_transfer, bank_account1, bank_account2)
+    transaction.transfer(amount_to_transfer, bank_account1, bank_account2)
     assert bank_account1._balance == amount1 - amount_to_transfer
     assert bank_account2._balance == amount_to_transfer
-    spy.assert_called_once()
+    assert spy.call_count == 3
 
 # testar logger för sig
-def test_logger__spy_logs_transaction(mocker):
-    logger = Logger()
+def test_logger__spy_logs_transaction(mocker, logger):
     spy = mocker.spy(logger, 'log')
-
     logger.log("Test")
     assert spy.call_count == 1
